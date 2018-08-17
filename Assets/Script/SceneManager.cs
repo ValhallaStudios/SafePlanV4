a69c036d62f5fdb154ff7580fcb88b13c0079a4a
Logon Aluno <grupodealunosdafiap@gmail.com>
2018-08-17 10:03:32 -0300

    Initial commit

---
+++ Assets/Script/SceneManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SceneManager : MonoBehaviour {
+
+    public Animator anima;
+    public string name;
+    public GameObject target;
+    public Text txtTimer;
+    public  int timer;
+
+    // Use this for initialization
+    void Start () {
+       name = target.tag.ToString();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+
+    public void chamarAnim() {
+        StartCoroutine("Anim");
+    }
+
+    public void BackMenu() {
+        Application.LoadLevel("menu");
+    }
+
+    public void BackGameMode()
+    {
+        Application.LoadLevel("GameMode");
+    }
+    IEnumerator Anim()
+    {
+        anima.SetBool("fade", true);
+        
+        yield return new WaitForSeconds(0.45f);
+        
+        Application.LoadLevel(name);
+
+        yield return new WaitForSeconds(1.0f);
+        StartCoroutine(cronometro(timer));
+
+    }
+
+    
+    }
---
+++ Assets/game.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class game : MonoBehaviour {
+    public int timer;
+    public Text txtTimer;
+	// Use this for initialization
+	void Start () {
+        txtTimer.text = timer.ToString();
+        StartCoroutine(cronometro(timer));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    IEnumerator cronometro(int _timer)
+    {
+        
+        txtTimer.text = timer.ToString();
+        timer--;
+        print("teste");
+        yield return new WaitForSeconds(1.0f);
+        StartCoroutine(cronometro(timer));
+    }
+}
