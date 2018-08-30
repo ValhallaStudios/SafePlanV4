8140ec2b58ed95987b48efdcd4c683f96e00471e
GustavoDoll <grupodealunosdafiap@gmail.com>
2018-08-30 12:01:14 -0300

    Criação da tela de Quiz

---
+++ Assets/Script/Quiz.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quiz : MonoBehaviour {
+    public List<string> alternativas;
+    public List<Text> alternativasTexto;
+    int limite = 5;
+   
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        SortearAlternativas();
+        
+    }
+
+
+    void SortearAlternativas() {
+        foreach (var alt in alternativas)
+        {
+            int index = UnityEngine.Random.Range(0, limite);
+             alternativasTexto[index].text = alt.ToString();
+            alternativasTexto.RemoveAt(index);
+            limite--;
+            alternativas.Remove(alt);
+        }
+        if (alternativas.Count <=0)
+        {
+            
+            RespostaCerta();
+        }
+    }
+
+    private void RespostaCerta()
+    {
+        //Achar a alternativa Correta via tag com id da pergunta
+    }
+}
